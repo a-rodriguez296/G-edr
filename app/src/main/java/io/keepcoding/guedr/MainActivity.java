1167fb38fb20e5f4a36c9d60c8c63d2ff55332fe
Juan José Ramírez <juanjose.ramirez@gmail.com>
2015-09-07 22:56:06 +0200

    2 offline weather systems with toggle

--- app/src/main/java/io/keepcoding/guedr/MainActivity.java
+++ app/src/main/java/io/keepcoding/guedr/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
 
@@ -17,20 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mImageView = (ImageView) findViewById(R.id.weather_image);
-        findViewById(R.id.system1_button).setOnClickListener(this);
-        findViewById(R.id.system2_button).setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.system1_button:
-                mImageView.setImageResource(R.drawable.offline_weather);
-                break;
-            case R.id.system2_button:
-                mImageView.setImageResource(R.drawable.offline_weather2);
-                break;
-        }
+        final ToggleButton button = (ToggleButton) findViewById(R.id.change_system_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImageView.setImageResource(button.isChecked()? R.drawable.offline_weather : R.drawable.offline_weather2 );
+            }
+        });
     }
 }
