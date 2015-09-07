fe55027b974bfb533f6a3131469456b30fee6c13
Juan José Ramírez <juanjose.ramirez@gmail.com>
2015-09-07 22:17:26 +0200

    2 offline weather systems

--- app/src/main/java/io/keepcoding/guedr/MainActivity.java
+++ app/src/main/java/io/keepcoding/guedr/MainActivity.java
@@ -6,22 +6,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView imageView = (ImageView) findViewById(R.id.weather_image);
-        Button changeButton = (Button) findViewById(R.id.change_system_button);
-        changeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageView.setImageResource(R.drawable.offline_weather2);
-            }
-        });
+        mImageView = (ImageView) findViewById(R.id.weather_image);
+        findViewById(R.id.system1_button).setOnClickListener(this);
+        findViewById(R.id.system2_button).setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.system1_button:
+                mImageView.setImageResource(R.drawable.offline_weather);
+                break;
+            case R.id.system2_button:
+                mImageView.setImageResource(R.drawable.offline_weather2);
+                break;
+        }
+    }
 }
